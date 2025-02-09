84b8cc945a53af6a62797e227300ab134ff82243
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-02-09 19:58:10 +0530

    Learning the concept of alert dialog boX

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterconcepts/widgets/ListGridviews.dart';
+import 'package:flutterconcepts/widgets/alert.dart';
 import 'package:flutterconcepts/widgets/drawer.dart';
-import 'package:flutterconcepts/widgets/image.dart';
+import 'package:flutterconcepts/widgets/imagetest.dart';
 import 'package:flutterconcepts/widgets/rowsandcols.dart';
 import 'package:flutterconcepts/widgets/button.dart';
 import 'package:flutterconcepts/widgets/snackbar.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.blue,
       ),
-      home: ImageWidgettest(),
+      home: alertdialogtest(),
     );
   }
 }
