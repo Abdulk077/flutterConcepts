fc0d11ced5578b0feb1e7a9d740d5a7b38b5d56c
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-03-01 15:36:47 +0530

    Implementing Icons Of the App

---
+++ lib/widgets/AnimatedSplashScreentest.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Animatedtest extends StatelessWidget {
+  const Animatedtest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
